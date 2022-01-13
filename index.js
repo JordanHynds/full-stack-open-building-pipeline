7c5f6be1579de94f0e446cb89b597dcee53d518f
JordanHynds <jordanroberthynds@gmail.com>
2022-01-13 10:49:34 -0800

    building pipeline

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('./app')
 const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 3001
 const server = http.createServer(app)
 
 server.listen(PORT, () => {
