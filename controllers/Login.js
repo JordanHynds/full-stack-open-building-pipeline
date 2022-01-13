68fbf205dad1098a32b20aee1d8af6881604ec93
JordanHynds <jordanroberthynds@gmail.com>
2022-01-13 15:47:46 -0800

    config

--- controllers/Login.js
+++ controllers/Login.js
@@ -4,6 +4,7 @@ const loginRouter = require('express').Router()
 const User = require('../models/user')
 
 loginRouter.post('', async (request, response) => {
+    console.log("works")
     const body = request.body
     const user = await User.findOne({ username: body.username })
     const passwordCorrect = user === null
