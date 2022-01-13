f34aeebe04318159a5be45544d30d5d36618dab4
JordanHynds <jordanroberthynds@gmail.com>
2022-01-13 12:07:12 -0800

    config

--- bloglist-backend/index.js
+++ bloglist-backend/index.js
@@ -3,9 +3,10 @@ const app = require('./app')
 const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const PORT = process.env.PORT || 3001
+
 const server = http.createServer(app)
 
+const PORT = process.env.PORT || 3001
 server.listen(PORT, () => {
     logger.info(`Server running on port ${PORT}`)
 })
