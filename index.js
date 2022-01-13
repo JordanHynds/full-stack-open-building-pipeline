8c3ff1ed7e0c2acfef08f6c08e865240ffdc8f07
JordanHynds <jordanroberthynds@gmail.com>
2022-01-13 10:47:01 -0800

    building pipeline

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = require('./app')
 const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-
+const PORT = process.env.PORT || 5001
 const server = http.createServer(app)
 
-server.listen(config.PORT, () => {
-    logger.info(`Server running on port ${config.PORT}`)
+server.listen(PORT, () => {
+    logger.info(`Server running on port ${PORT}`)
 })
 
