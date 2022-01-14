2002c60e317b6c11993172c0204906615a2319d2
JordanHynds <jordanroberthynds@gmail.com>
2022-01-13 16:05:50 -0800

    config

--- controllers/Login.js
+++ controllers/Login.js
@@ -7,6 +7,7 @@ loginRouter.post('', async (request, response) => {
     const body = request.body
     console.log(body)
     const user = await User.findOne({ username: body.username })
+    console.log(user)
     const passwordCorrect = user === null
         ? false
         : await bcrypt.compare(body.password, user.passwordHash)
