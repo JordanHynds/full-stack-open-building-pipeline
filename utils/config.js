fa3214336392316979e6f3ee7ba422f261e2de30
JordanHynds <jordanroberthynds@gmail.com>
2022-01-20 12:46:09 -0800

    fixed

--- utils/config.js
+++ utils/config.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 const PORT = process.env.PORT
 
 const mongoUri = process.env.NODE_ENV === "test"
-    ? process.env.test_mongo_Uri
+    ? process.env.TEST_MONGO_URI
     : process.env.MONGO_URI
 
 module.exports = {
